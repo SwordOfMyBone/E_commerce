f28e2881b2b818ef661b639662aa9bcb352eb05f
Mazin Al Haddad <mazinalhaddad05@gmail.com>
2022-09-05 10:30:50 +0300

    NOW WORKING ORDERS MODEL

--- e_commerce_store/orders/forms.py
+++ e_commerce_store/orders/forms.py
@@ -6,4 +6,4 @@ from .models import Order
 class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ["user"]
+        fields = ["address", "postal_code", "city"]
--- e_commerce_store/orders/views.py
+++ e_commerce_store/orders/views.py
@@ -1,19 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from e_commerce_store.cart.cart import Cart
 
 from .forms import OrderCreateForm
 from .models import OrderItem
+from .tasks import order_created
 
 # Create your views here.
 
 
+@login_required
 def order_create(request):
     cart = Cart(request)
     if request.method == "POST":
         form = OrderCreateForm(request.POST)
         if form.is_valid():
-            order = form.save()
+            order = form.save(commit=False)
+            order.user = request.user
+            order.save()
             for item in cart:
                 OrderItem.objects.create(
                     order=order,
@@ -23,6 +28,7 @@ def order_create(request):
                 )
             # clear the cart
             cart.clear()
+            order_created.delay(order.id)
             return render(request, "orders/order/created.html", {"order": order})
     else:
         form = OrderCreateForm()
